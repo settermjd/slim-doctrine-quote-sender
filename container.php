08ed7c847beb1ad5e7a3c133be59bd4a6fde386c
Matthew Setter <matthew@matthewsetter.com>
2023-02-08 10:20:39 +0100

    Update the container for the new classes
    This registers the required services with the container so that all ofa
    the applicable classes can be loaded, when needed.

--- container.php
+++ container.php
@@ -1,11 +1,25 @@
 <?php
 
+use App\Handler\Subscribe\SubscribeByEmailFormHandler;
+use App\Handler\Subscribe\SubscribeByEmailHandler;
+use App\Handler\Subscribe\SubscribeByMobileHandler;
+use App\Handler\Unsubscribe\UnsubscribeByEmailFormHandler;
+use App\Handler\Unsubscribe\UnsubscribeByEmailHandler;
+use App\Handler\Unsubscribe\UnsubscribeByMobileHandler;
+use App\InputFilter\EmailInputFilter;
+use App\InputFilter\MobileNumberInputFilter;
+use App\QuoteService;
 use App\UserService;
 use DI\Container;
 use Doctrine\Common\Cache\Psr6\DoctrineProvider;
 use Doctrine\DBAL\Types\Type;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Tools\Setup;
+use Mezzio\Session\Ext\PhpSessionPersistenceFactory;
+use Mezzio\Session\SessionMiddleware;
+use Mezzio\Session\SessionMiddlewareFactory;
+use Mezzio\Session\SessionPersistenceInterface;
+use Mezzio\Template\TemplateRendererInterface;
 use Symfony\Component\Cache\Adapter\ArrayAdapter;
 use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
@@ -38,8 +52,60 @@ $container->set(EntityManager::class, static function (Container $c): EntityMana
     return EntityManager::create($settings['doctrine']['connection'], $config);
 });
 
-$container->set(UserService::class, static function (Container $c) {
-    return new UserService($c->get(EntityManager::class));
+$container->set(SessionPersistenceInterface::class, static function(Container $c): SessionPersistenceInterface {
+    $sessionPersistence = new PhpSessionPersistenceFactory();
+    return $sessionPersistence($c);
+});
+
+$container->set(SessionMiddleware::class, static function(Container $c): SessionMiddleware {
+    $sessionMiddleware = new SessionMiddlewareFactory();
+    return $sessionMiddleware($c);
+});
+
+$container->set(UserService::class, static function(Container $c): UserService {
+    /** @var EntityManager $em */
+    $em = $c->get(EntityManager::class);
+    return new UserService($em);
+});
+
+$container->set(QuoteService::class, static function(Container $c): QuoteService {
+    /** @var EntityManager $em */
+    $em = $c->get(EntityManager::class);
+    return new QuoteService($em);
+});
+
+$container->set(UnsubscribeByMobileHandler::class, static function (Container $c): UnsubscribeByMobileHandler {
+    /** @var UserService $userService */
+    $userService = $c->get(UserService::class);
+    return new UnsubscribeByMobileHandler($userService, new MobileNumberInputFilter());
+});
+
+$container->set(UnsubscribeByEmailHandler::class, static function (Container $c): UnsubscribeByEmailHandler {
+    /** @var UserService $userService */
+    $userService = $c->get(UserService::class);
+    return new UnsubscribeByEmailHandler($userService, new EmailInputFilter());
+});
+
+$container->set(UnsubscribeByEmailFormHandler::class, static function (Container $c): UnsubscribeByEmailFormHandler {
+    /** @var TemplateRendererInterface $templateRenderer */
+    $templateRenderer = $c->get(TemplateRendererInterface::class);
+    return new UnsubscribeByEmailFormHandler($templateRenderer);
+});
+
+$container->set(SubscribeByEmailFormHandler::class, static function (Container $c): SubscribeByEmailFormHandler {
+    return new SubscribeByEmailFormHandler();
+});
+
+$container->set(SubscribeByMobileHandler::class, static function (Container $c): SubscribeByMobileHandler {
+    /** @var UserService $userService */
+    $userService = $c->get(UserService::class);
+    return new SubscribeByMobileHandler($userService, new MobileNumberInputFilter());
+});
+
+$container->set(SubscribeByEmailHandler::class, static function (Container $c): SubscribeByEmailHandler {
+    /** @var UserService $userService */
+    $userService = $c->get(UserService::class);
+    return new SubscribeByEmailHandler($userService, new EmailInputFilter());
 });
 
 return $container;
